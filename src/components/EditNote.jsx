161e56c18acd291d3e75aa5923dfc32d9ebbbd2d
Koushik <koushik.mbp@gmail.com>
2024-04-10 00:48:14 +0530

    'Initial'

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import NoteComponent from './NoteComponent'
 
 const EditNote = () => {
-  return (
-    <div>EditNote</div>
-  )
+    const params = useParams()
+    const [note, setNote] = useState({})
+    useEffect(() => {
+        axios.get('http://localhost:4000/notes/' + params.id)
+            .then(response => {
+                setNote(response.data.note)
+                console.log(response.data)
+            })
+    }, [])
+
+
+    return (
+        <div>
+            <NoteComponent note={note} />
+        </div>
+    )
 }
 
 export default EditNote
--- src/components/NoteComponent.jsx
+++ src/components/NoteComponent.jsx
@@ -4,10 +4,19 @@ import axios from 'axios'
 const NoteComponent = (props) => {
     const [note, setNote] = useState()
     const [loading, setLoading] = useState(true)
+    const [changes, setChanges] = useState({ title: '', content: '' })
+    const [changed, setChanged] = useState()
     useEffect(() => {
         setNote(props.note);
-        setLoading(false);
-    }, [])
+        setChanges(props.note);
+        console.log(props.note)
+        setLoading(false)
+    }, [props])
+
+    useEffect(() => {
+      console.log(changes)
+    }, [changes])
+    
 
     const deleteNote = async (id) => {
         console.log(id);
@@ -21,20 +30,34 @@ const NoteComponent = (props) => {
     return (
         <div>
             {!loading &&
-                    <div className='m-3'>
-                        <h3 className='font-bold'>{note.title}</h3>
-                        <div role='textbox'>{note.content}</div>
-                        <p>{note.updatedAt}</p>
-                        <div className='flex gap-4'>
-                            <button onClick={() => deleteNote(note._id)} className='text-blue-500 underline'>
-                                {/* <img src={('../../public/delete-icon.png')} alt='DELETE' className='size-4' /> */}
-                                DELETE
-                            </button>
-                            <a href={`/note/${note._id}`}>
-                                <img src={require('../assets/images/open-icon.webp')} alt='OPEN' className='size-4 relative top-1' />
-                            </a>
-                        </div>
+                <div className='m-3'>
+                    <h3 className='font-bold' contentEditable >{note.title}</h3>
+                    <textarea role='textbox' contentEditable
+                        className='w-5/6 min-h-5 max-h-full'
+                        value={changes.content ||  note.content}
+                        onChange={(e) => {
+                            setChanges({ ...changes, content: e.target.value });
+                            setChanged(true)
+                        }}>
+                        {note.content}
+                        </textarea>
+                    <p>{note.updatedAt}</p>
+                    <div className='flex gap-4'>
+                        <button onClick={() => deleteNote(note._id)} className='text-blue-500 underline'>
+                            {/* <img src={('../../public/delete-icon.png')} alt='DELETE' className='size-4' /> */}
+                            DELETE
+                        </button>
+                        <a href={`/note/${note._id}`}>
+                            <img src={require('../assets/images/open-icon.webp')} alt='OPEN' className='size-4 relative top-1' />
+                        </a>
                     </div>
+                </div>
+            }
+            {
+                changed &&
+                <>
+                    <button className='border-2 border-blue-800'>SAVE</button>
+                </>
             }
         </div>
     )
